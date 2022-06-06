6e88514fb80e3a7a85eca8a9793cb44e959105ea
Tramio <tramiodev@outlook.fr>
2022-06-06 16:35:10 +0200

    [addition] nav component: add more tabs, add router Link to "Try Notion free" button [styling] nav component: add styling [routing] create SignUp.js and add it to RouteSwitch.js

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import App from "./App";
+import SignUp from "./components/SignUp";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
-    <Nav />
+      <Nav />
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+const SignUp = () => {
+
+  return (
+    <div>
+      <h1 className="signup-greeting">
+        Sign up
+      </h1>
+    </div>
+  )
+}
+
+export default SignUp;
